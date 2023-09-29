7574bc4140e5a6113ef088944394d0c1bbd56247
Oleksii Kluban <jsweetpirozhok@gmail.com>
2023-09-29 22:06:19 +0300

    add routs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { Observable } from 'rxjs';
+import { Vessel } from './models/vessel.models';
+import { VesselFacade } from './services/vessel.facade';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MenubarModule],
+  imports: [CommonModule, RouterModule, MenubarModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   items: MenuItem[] = [
     {
-      label: 'Vessels'
+      label: 'Vessels',
+      routerLink: 'vessels'
     },
     {
-      label: 'Emissions '
+      label: 'Emissions',
+      routerLink: 'emissions'
     }
   ];
+
+  vessels$!: Observable<Vessel[]>;
+
+  constructor(private vesselFacade: VesselFacade) {}
+
+  ngOnInit(): void {
+    this.vesselFacade.load();
+
+    this.vessels$ = this.vesselFacade.allVessel$;
+
+  }
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('modules/vessels').then(m => m.VesselsComponent)
+  },
+  {
+    path: 'vessels',
+    loadComponent: () => import('modules/vessels').then(m => m.VesselsComponent)
+  },
+  {
+    path: 'emissions',
+    loadComponent: () => import('modules/emissions').then(m => m.EmissionsComponent)
+  }
+];
