ebc73b791f56c4f25fb243e20a643e88283548f0
Oleksii Kluban <jsweetpirozhok@gmail.com>
2023-10-02 04:37:05 +0300

    feat(vessels): implement lib-emissions

--- modules/vessels/src/lib/emissions/emissions.component.spec.ts
+++ modules/vessels/src/lib/emissions/emissions.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { EmissionsChartComponent } from '../emissions-chart/emissions-chart.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { EmissionsFacade } from '../services/emissions.facade';
+import { VesselFacade } from '../services/vessel.facade';
 import { EmissionsComponent } from './emissions.component';
 
 describe('EmissionsComponent', () => {
@@ -8,8 +12,8 @@ describe('EmissionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EmissionsComponent],
-      providers: [provideMockStore({})],
+      imports: [EmissionsComponent, DropdownModule, EmissionsChartComponent],
+      providers: [provideMockStore({}), VesselFacade, EmissionsFacade],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmissionsComponent);
--- modules/vessels/src/lib/emissions/emissions.component.ts
+++ modules/vessels/src/lib/emissions/emissions.component.ts
@@ -1,21 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { selectEntity } from '../state/emissions/emissions.selectors';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { filter, withLatestFrom } from 'rxjs';
+import { EmissionsChartComponent } from '../emissions-chart/emissions-chart.component';
+import { Vessel } from '../models/vessel.models';
+import { EmissionsFacade } from '../services/emissions.facade';
+import { VesselFacade } from '../services/vessel.facade';
 
 @Component({
   selector: 'lib-emissions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DropdownModule, ReactiveFormsModule, EmissionsChartComponent],
   templateUrl: './emissions.component.html',
   styleUrls: ['./emissions.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EmissionsComponent {
-  private readonly store = inject(Store);
-  emissions$ = this.store.pipe(select(selectEntity))
+export class EmissionsComponent implements OnInit {
+  vesselFacade = inject(VesselFacade);
+  emissionsFacade = inject(EmissionsFacade);
+  selectedEmission$ = this.emissionsFacade.selectedEmission$;
+  vesselControl = new FormControl<Vessel | null>(null);
+  vessels: Vessel[] = [];
+  selectedVessel!: Vessel;
+  private destroyRef = inject(DestroyRef);
 
+  ngOnInit(): void {
+    this.vesselFacade.loadOnce();
+    this.emissionsFacade.loadOnce();
 
+    this.vesselControl.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(val => {
+      val && this.vesselFacade.select(val.id);
+    });
 
+    this.vesselFacade.allVessel$.pipe(
+      filter(e => !!e),
+      withLatestFrom(this.vesselFacade.selectedVessel$),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(([vessels, selected]) => {
+      this.vessels = vessels;
+      this.selectedVessel = selected ?? this.vessels[0];
+      this.vesselControl.setValue(this.selectedVessel);
+    });
+
+    this.vesselFacade.selectedVessel$.pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(vessel => {
+      this.selectedVessel = vessel ?? this.vessels[0];
+    });
+  }
 
 }
